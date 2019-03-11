0413e1af4fe1c69c3d9659f529417eeb4c7401d4
Bonita Choi <bonitac93@gmail.com>
2019-03-11 18:55:14 +0000

    created module for the function

---
+++ https-functions.js
@@ -0,0 +1,21 @@
+module.exports = function getHTML (options, callback) {
+  /* Your code here */
+  var https = require('https');
+  var ret = ''
+  https.get(options, function (response) {
+    response.setEncoding('utf8'); // set encoding of received data to UTF-8
+    response.on('data', function (data) { // the callback is invoked when a `data` chunk is received
+      ret += callback(data);
+    });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+    response.on('end', function() {
+      return ret;
+    });
+  });  
+};
+
+function printHTML (html) {
+  console.log(html);
+}
