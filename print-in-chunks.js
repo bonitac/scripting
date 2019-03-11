618222e375bcca925a51e6d2bf9d3003da14fa40
Bonita Choi <bonitac93@gmail.com>
2019-03-11 18:32:51 +0000

    fixed indentations

--- print-in-chunks.js
+++ print-in-chunks.js
@@ -1,31 +1,30 @@
 var https = require('https');
 
 function getAndPrintHTMLChunks () {
-
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
 
-  // notice that https.get takes a callback with one parameter -
+// notice that https.get takes a callback with one parameter -
 // response, which is a Stream that represents the HTTP response
-https.get(requestOptions, function (response) {
+  https.get(requestOptions, function (response) {
 
   // set encoding of received data to UTF-8
-  response.setEncoding('utf8');
+    response.setEncoding('utf8');
 
   // the callback is invoked when a `data` chunk is received
-  response.on('data', function (data) {
-    console.log(data + "\n");
-  });
+    response.on('data', function (data) {
+      console.log(data + "\n");
+    });
 
   // the callback is invoked when all of the data has been received
   // (the `end` of the stream)
-  response.on('end', function() {
-    console.log('Response stream complete.');
-  });
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
 
-});
+  });
   /* Add your code here */
 
 }
